5e922ee6fa3c96e3908a5a44037456bd0bef8f9d
PaulZhe <674998795@qq.com>
2019-11-24 21:54:31 +0800

    maopao

--- 排序算法/冒泡排序.cpp
+++ 排序算法/冒泡排序.cpp
@@ -12,9 +12,9 @@ void BubbleSort(vector<int> &arr) {
 	bool flag = true;
 		for (int i = 0; i < arr.size() - 1 && flag; i++) {
 			flag = false;
-			for (int j = i + 1; j < arr.size(); j++) {
-				if (arr[i] > arr[j]) {
-					swap(arr[i], arr[j]);
+			for (int j = 0; j < arr.size() - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					swap(arr[j], arr[j + 1]);
 					flag = true;
 				}
 			}
